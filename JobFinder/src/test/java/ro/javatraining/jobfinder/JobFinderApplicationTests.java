fce3ff2056a3de3965750c959652163755b1d399
Achitei Marius <marius.achitei02@gmail.com>
2023-12-10 22:41:41 +0200

    initial commit

---
+++ JobFinder/src/test/java/ro/javatraining/jobfinder/JobFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.javatraining.jobfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
