c251837a3e1a3fa17803f0c5c8e541357ada4da4
Achitei Marius <marius.achitei02@gmail.com>
2023-12-14 12:19:51 +0200

    status

--- JobFinder/src/main/java/ro/javatraining/jobfinder/user/UserController.java
+++ JobFinder/src/main/java/ro/javatraining/jobfinder/user/UserController.java
@@ -1,6 +1,7 @@
 package ro.javatraining.jobfinder.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import ro.javatraining.jobfinder.user.api.RegisterUserDto;
@@ -16,26 +17,31 @@ public class UserController {
     UserManagement userManagement;
 
     @GetMapping("")
+    @ResponseStatus(value = HttpStatus.OK)
     public Collection<UserDto> getAll() {
         return userManagement.getAll();
     }
 
     @GetMapping("/{id}")
+    @ResponseStatus(value = HttpStatus.OK)
     public UserDto getById(@PathVariable Long id) {
         return userManagement.get(id);
     }
 
     @PostMapping("")
+    @ResponseStatus(value = HttpStatus.CREATED)
     public Long create(RegisterUserDto registerUserDto) {
         return userManagement.registerUser(registerUserDto);
     }
 
     @PutMapping("/{id}")
+    @ResponseStatus(value = HttpStatus.OK)
     public void update(@PathVariable Long id, UserDto userDto) {
         userManagement.update(id, userDto);
     }
 
     @DeleteMapping("/{id}")
+    @ResponseStatus(value = HttpStatus.OK)
     public void delete(@PathVariable Long id) {
         userManagement.delete(id);
     }
